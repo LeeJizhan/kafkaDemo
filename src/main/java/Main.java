b6813fb5b41d7a7fca69644e342750480b7fb6b0
LeeJizhan <15jzli1@stu.edu.cn>
2018-08-10 17:06:16 +0800

    开启工作入口

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 import drools.droolsbean.CarStatusRuturn;
 import drools.monitor.Monitor;
-import org.apache.derby.iapi.store.raw.log.Logger;
+import kafka.gpstest.Producer;
+import properties.KafkaProperties;
+import spark.SparkMain;
+import spark.SparkOper;
 import utils.LoggerUtil;
 
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Asus- on 2018/7/12.
@@ -17,50 +21,54 @@ public class Main {
 //        dbOper.updateCarData();
 //        dbOper.updateGpsDeviceData();
 //        dbOper.updateCarAndGpsData();
-        String[] carLons = {"122.04", "121.10", "112.10"};
-        String[] carLats = {"22.04", "21.10", "12.10"};
+
+//        String[] carLons = {"121.468100", "121.470000", "121.583500"};
+//        String[] carLats = {"31.291600", "31.198000", "31.221100"};
+//
+//        Monitor monitor = new Monitor();
+//        monitor.setPriority(10);
+//        monitor.start();
+//        Thread.sleep(10000);
+//        //Thread.currentThread().join();
+//        for (int i = 0; i < 1000; i++) {
+//            try {
+//                List<CarStatusRuturn> list = monitor.getMonitorAndData(carLons[i % 3], carLats[i % 3]);
+//                for (CarStatusRuturn carStatusRuturn : list) {
+//                    LoggerUtil.info("id:  " + carStatusRuturn.getwLID()
+//                            + "  status: " + carStatusRuturn.getcStatus());
+//                }
+//                System.out.println("-------------------------------------------");
+//                System.out.println("-------------------------------------------");
+//                System.out.println("-------------------------------------------");
+//                Thread.sleep(3000);
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
         Monitor monitor = new Monitor();
         monitor.setPriority(10);
         monitor.start();
-        Thread.sleep(10000);
-        //Thread.currentThread().join();
-        for (int i = 0; i < 300; i++) {
-            try {
-                List<CarStatusRuturn> list = monitor.getMonitorAndData(carLons[0], carLats[0]);
-                for (CarStatusRuturn carStatusRuturn : list) {
-                    LoggerUtil.info("id:  " + carStatusRuturn.getwLID()
-                            + "  status: " + carStatusRuturn.getcStatus());
-                }
-                System.out.println("-------------------------------------------");
-                Thread.sleep(3000);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+//        Thread.sleep(10000);
+        KafkaProperties kafkaProperties = KafkaProperties.getInstance();
+        //通过键值对的方式读取kafka配置文件的值
+        String gpsTopic = kafkaProperties.getKafkaGpsTopic();
+        boolean isAsync = true;
+        //从数据库拿数据扔到kafka中
+        Producer producer = new Producer(gpsTopic, isAsync);
+        producer.start();
 
+        //Spark Streaming从kafka拿数据进行实时处理写入HBase中
+        SparkMain sparkMain = new SparkMain();
+        sparkMain.doWork();
 
-        /**
-         KafkaProperties kafkaProperties = KafkaProperties.getInstance();
-         //通过键值对的方式读取kafka配置文件的值
-         String gpsTopic = kafkaProperties.getKafkaGpsTopic();
-         boolean isAsync = true;
-         //从数据库拿数据扔到kafka中
-         Producer producer = new Producer(gpsTopic, isAsync);
-         producer.start();
-
-         //Spark Streaming从kafka拿数据进行实时处理写入HBase中
-         SparkMain sparkMain = new SparkMain();
-         sparkMain.run();
-
-         //定时任务，进行常驻点分析
-         Runnable runnable = () -> {
-         SparkOper oper = new SparkOper();
-         oper.doWork();
-         };
-         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-         //从执行开始的时间起，24小时更新一次，initialDelay表示延迟1s后开始第一次执行
-         service.scheduleAtFixedRate(runnable, 1, 3600 * 24, TimeUnit.SECONDS);
-         **/
+        //定时任务，进行常驻点分析
+        Runnable runnable = () -> {
+            SparkOper oper = new SparkOper();
+            oper.doWork();
+        };
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        //从执行开始的时间起，24小时更新一次，initialDelay表示延迟一天后开始第一次执行
+        service.scheduleAtFixedRate(runnable, 3600 * 24, 3600 * 24, TimeUnit.SECONDS);
     }
 }
