2185e4de6e95102f38a8d8168de8d8fd028a4ea4
LeeJizhan <15jzli1@stu.edu.cn>
2018-08-14 17:52:24 +0800

    优化停车点操判断

--- src/main/java/spark/SparkOper.java
+++ src/main/java/spark/SparkOper.java
@@ -1,6 +1,7 @@
 package spark;
 
 import hbase.HBaseOper;
+import utils.LoggerUtil;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -32,12 +33,17 @@ public class SparkOper {
         String gpsFamily = "gpsinfo";
         String usualFamily = "usualstopinfo";
         int count = 2350;
+//        Map<String, Object> myMap = new HashMap<>();
+//        myMap.put("lon", 121.382333);
+//        myMap.put("lat", 31.192717);
+//        myMap.put("totalTime", 3600 * 24);
+//        oper.insert(tableName, Integer.toString(375), usualFamily, myMap);
         /**
          * 1.获取HBase中的数据
          * 2.将HBase获取到的数据
          */
         //1.获取HBase中的数据
-        for (int i = 1; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             Map<String, List<String>> myListMap = oper.getAllDataByRowKeyAndFamily(tableName, Integer.toString(i + 1), gpsFamily);
             for (Map.Entry entry : myListMap.entrySet()) {
                 String key = (String) entry.getKey();
@@ -57,6 +63,7 @@ public class SparkOper {
             String totalStopTime = "";
             //默认是运动的
             boolean isStop = false;
+            boolean isChange = false;
             String stopLon = "";
             String stopLat = "";
 
@@ -74,6 +81,7 @@ public class SparkOper {
                     //停止到运动，记录当前时间，将停车点和停车时间写进HBase
                     if (Integer.valueOf(speedList.get(j)) > 0) {
                         isStop = false;
+                        isChange = true;
                         finalStopTime = timeList.get(j);
                         try {
                             long start = simpleDateFormat.parse(startStopTime).getTime();
@@ -88,17 +96,33 @@ public class SparkOper {
                         myMap.put("lat", stopLat);
                         myMap.put("totalTime", totalStopTime);
                         oper.insert(tableName, Integer.toString(i + 1), usualFamily, myMap);
-                        System.out.println("stopLon:" + stopLon
+                        LoggerUtil.info("stopLon:" + stopLon
                                 + "   stopLat:" + stopLat
                                 + "   totalStopTime:" + totalStopTime + " s");
                     }
                 }
             }
+            //表示速度一直为0
+            if (isStop == true && isChange == false) {
+                Map<String, Object> myMap = new HashMap<>();
+                myMap.put("lon", lonList.get(0));
+                myMap.put("lat", latList.get(0));
+                myMap.put("totalTime", 3600 * 24);
+                oper.insert(tableName, Integer.toString(i + 1), usualFamily, myMap);
+            }
+            //表示速度一直不变
+            if (isStop == false && isChange == false) {
+                Map<String, Object> myMap = new HashMap<>();
+                myMap.put("lon", lonList.get(0));
+                myMap.put("lat", latList.get(0));
+                myMap.put("totalTime", 3600 * 24);
+                oper.insert(tableName, Integer.toString(i + 1), usualFamily, myMap);
+            }
         }
     }
 
-//    public static void main(String[] args) {
-//        SparkOper sparkOper = new SparkOper();
-//        sparkOper.start();
-//    }
+    public static void main(String[] args) {
+        SparkOper sparkOper = new SparkOper();
+        sparkOper.doWork();
+    }
 }
