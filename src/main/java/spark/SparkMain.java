042b6b762eeaa606d5ee7acc738f6124d2a8bdd4
LeeJizhan <15jzli1@stu.edu.cn>
2018-08-16 14:34:14 +0800

    添加报警信息写入MySQL任务

--- src/main/java/spark/SparkMain.java
+++ src/main/java/spark/SparkMain.java
@@ -1,7 +1,10 @@
 package spark;
 
 import bean.GpsDataBean;
+import bean.MsgBean;
+import bean.MsgDataBean;
 import com.google.gson.Gson;
+import db.DBCon;
 import drools.droolsbean.CarStatusRuturn;
 import drools.monitor.Monitor;
 import hbase.HBaseOper;
@@ -17,21 +20,29 @@ import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
 import properties.HBaseTableProperties;
 import properties.KafkaProperties;
-import utils.LoggerUtil;
 
 import java.beans.Transient;
+import java.io.Serializable;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 /**
  * Created by Asus- on 2018/8/2.
  * Spark Streaming 实时数据处理
  */
-public class SparkMain {
+public class SparkMain implements Serializable {
 
     private static JavaStreamingContext javaStreamingContext;
     private static JavaInputDStream<ConsumerRecord<String, String>> stream;
+
+    private SparkConf sparkConf;
     private static Gson gson = new Gson();
+
+    //mysql
+    private static transient DBCon dbCon;
+    private static transient Connection connection;
 
     //HBase
     private static String tableName;
@@ -39,7 +50,12 @@ public class SparkMain {
     private static String etcFamily;
     private static HBaseOper hBaseOper;
 
+    //车辆状态 map
+    private static transient Map<Integer, String> stateMap = new HashMap<>();
+
     public SparkMain() {
+        dbCon = DBCon.getInstance();
+        connection = dbCon.getConnection();
         initSparkStreaming();
         initHBase();
         stream = getKafkaData();
@@ -59,7 +75,7 @@ public class SparkMain {
      * 初始化SparkStreaming
      */
     private void initSparkStreaming() {
-        SparkConf sparkConf = new SparkConf().setMaster("local[2]").setAppName("calgpsdata");
+        sparkConf = new SparkConf().setMaster("local[2]").setAppName("calgpsdata");
         JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
         javaStreamingContext = new JavaStreamingContext(javaSparkContext, new Duration(5000));
     }
@@ -106,8 +122,54 @@ public class SparkMain {
                                 + "}";
                         Map<String, Object> msgMap = new HashMap<>();
                         msgMap.put("msg", fullMsg);
-                        //LoggerUtil.info(fullMsg);
+                        //插入车辆状态数据
                         hBaseOper.insert(tableName, gpsDataBean.getGpsid(), etcFamily, msgMap);
+
+                        //插入车辆报警信息
+                        //解析json，获取当前状态的数据
+                        MsgBean msgBeanNow = gson.fromJson(fullMsg, MsgBean.class);
+                        int carid = msgBeanNow.getCarid();
+                        String time = msgBeanNow.getTime();
+                        //存在carid才操作
+                        if (stateMap.containsKey(carid)) {
+                            //连接mysql将数据保存进去
+                            Statement statement;
+                            try {
+                                connection.setAutoCommit(false);
+                                statement = connection.createStatement();
+                                List<MsgDataBean> msgDataBeanNowList = msgBeanNow.getMsgDataBean();
+                                //获取上一状态的数据
+                                String oldJson = stateMap.get(carid);
+                                MsgBean msgBeanOld = gson.fromJson(oldJson, MsgBean.class);
+                                List<MsgDataBean> msgDataBeanOldList = msgBeanOld.getMsgDataBean();
+                                int size = msgDataBeanOldList.size() > msgDataBeanNowList.size() ? msgDataBeanNowList.size() : msgDataBeanOldList.size();
+                                //判断当前的状态和上一状态是否一致
+                                for (int i = 0; i < size; i++) {
+                                    if (!msgDataBeanNowList.get(i).getStatus().equals(msgDataBeanOldList.get(i).getStatus())) {
+                                        int status;
+                                        if (msgDataBeanNowList.get(i).getStatus().equals("WARN")) {//出
+                                            status = 0;
+                                        } else {//进
+                                            status = 1;
+                                        }
+                                        String fullSql = "insert into warntable(gpsid,ruleid,time,status) VALUES "
+                                                + "("
+                                                + "\'" + carid + "\'" + ","
+                                                + "\'" + msgDataBeanNowList.get(i).getRuleid() + "\'" + ","
+                                                + "\'" + time + "\'" + ","
+                                                + "\'" + status + "\'"
+                                                + ")";
+                                        statement.executeUpdate(fullSql);
+                                        connection.commit();
+                                    }
+                                }
+                                statement.close();
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        //更新map保存的状态
+                        stateMap.put(carid, fullMsg);
                     }
                 }
             });
@@ -158,4 +220,58 @@ public class SparkMain {
                 );
         return stream;
     }
+
+//    public void test(String fullMsg) {
+//        //插入车辆报警信息
+//        //1.解析json，获取当前状态的数据
+//        Gson gson = new Gson();
+//        MsgBean msgBeanNow = gson.fromJson(fullMsg, MsgBean.class);
+//        int carid = msgBeanNow.getCarid();
+//        String time = msgBeanNow.getTime();
+//        //存在carid才操作
+//        if (stateMap.containsKey(carid)) {
+//            //连接mysql将数据保存进去
+//            Statement statement = null;
+//            try {
+//                connection.setAutoCommit(false);
+//                statement = connection.createStatement();
+//                List<MsgDataBean> msgDataBeanNowList = msgBeanNow.getMsgDataBean();
+//                //获取上一状态的数据
+//                String oldJson = stateMap.get(carid);
+//                MsgBean msgBeanOld = gson.fromJson(oldJson, MsgBean.class);
+//                List<MsgDataBean> msgDataBeanOldList = msgBeanOld.getMsgDataBean();
+//                //判断当前的状态和上一状态是否一致，根据上一状态的长度
+//                for (int i = 0; i < msgDataBeanOldList.size(); i++) {
+//                    System.out.println(msgDataBeanNowList.get(i).getStatus() + "   " + msgDataBeanOldList.get(i).getStatus());
+//                    if (!msgDataBeanNowList.get(i).getStatus().equals(msgDataBeanOldList.get(i).getStatus())) {
+//                        int status;
+//                        if (msgDataBeanNowList.get(i).getStatus().equals("WARN")) {//出
+//                            status = 0;
+//                        } else {
+//                            status = 1;
+//                        }
+//
+//                        String fullSql = "insert into warntable(gpsid,ruleid,time,status) VALUES "
+//                                + "("
+//                                + "\'" + carid + "\'" + ","
+//                                + "\'" + msgDataBeanNowList.get(i).getRuleid() + "\'" + ","
+//                                + "\'" + time + "\'" + ","
+//                                + "\'" + status + "\'"
+//                                + ")";
+//                        statement.executeUpdate(fullSql);
+//                        connection.commit();
+//                    }
+//                }
+//                statement.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        //更新map保存的状态
+//        stateMap.put(carid, fullMsg);
+//    }
+//
+//    public static void main(String[] args) {
+//
+//    }
 }
